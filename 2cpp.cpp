094f226619bc15022fc4da778c71393ddca36bde
samudralaharshitha18-arch <samudralaharshitha18@gmail.com>
2025-11-25 11:53:28 +0530

    Add files via upload

---
+++ 2cpp.cpp
@@ -0,0 +1,16 @@
+//finding the area of circle //
+#include<stdio.h>
+int main()
+{
+	int a;
+	float area;
+	
+	
+	printf("enter radius");
+	scanf("%d",&a);
+	area=3.14*a*a;
+	printf("area=%f",area);
+	return 0;
+	
+	
+}
---
+++ 4 cpp.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main()
+{
+	float num1,num2,sum;
+	printf("enter first number:");
+	scanf("%f",&num1);
+	printf("enter second number:");
+	scanf("%f",&num2);
+	sum = num1 + num2;
+	printf ("result is:%f",sum);
+	return 0;
+	
+}
